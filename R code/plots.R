66a8241ff102626b24157053c48ed3cf354f005a
BaruchRosen <barucha_rosen13@hotmail.com>
2025-08-14 18:09:24 +0300

    final fixes

--- R code/plots.R
+++ R code/plots.R
@@ -1,29 +1,35 @@
 library(ggplot2)
-
+library(scales)
 geom_histogramPlot <- function(dataBase, country){
-  midBase <- dataBase[ which(dataBase$afterAccidentDays == 1 | dataBase$afterAccidentDays == 3) , ]
-
+  days = c(0, 1, 2, 3)
+  midBase <- dataBase[ dataBase$afterAccidentDays %in% days, ]
+  color_count <- length(unique(midBase$afterAccidentDays))
+  color_palette <- hue_pal()(color_count)
   
   if(country == "israel"){
-    ggplot(midBase, aes(x = revenue, fill=afterAccidentDays)) + geom_histogram(bins = 10,position = "dodge", alpha = 0.7)
+    ggplot(midBase, aes(x = revenue, fill=afterAccidentDays)) + geom_histogram(aes(y = ..density..),bins = 20,position = "dodge", alpha = 0.7)+
+      labs(y = "density (Israel)", fill = "After Accident Days")
   }else{
-    ggplot(midBase, aes(x = NYSE_revenue, fill=afterAccidentDays)) + geom_histogram(bins = 10,position = "dodge", alpha = 0.7)
+    ggplot(midBase, aes(x = NYSE_revenue, fill=afterAccidentDays)) + geom_histogram(aes(y = ..density..),bins = 20,position = "dodge", alpha = 0.7)+
+      labs(y = "density (USA)", fill = "After Accident Days")
   }
 }
 
 stat_ecdfPlot <- function(dataBase, country){
-  midBase <- dataBase[ which(dataBase$afterAccidentDays == 1 | dataBase$afterAccidentDays == 3) , ]
-  
+  days = c(0, 1, 2, 3)
+  midBase <- dataBase[ dataBase$afterAccidentDays %in% days, ]
+  color_count <- length(unique(midBase$afterAccidentDays))
+  color_palette <- hue_pal()(color_count)
   if(country == "israel"){
     ggplot(midBase, aes(x=revenue)) + stat_ecdf(aes(color = afterAccidentDays,linetype = afterAccidentDays), 
                                               geom = "step", size = 1.5) +
-    scale_color_manual(values = c("#00AFBB", "#E7B800"))+
-    labs(y = "Cumulative probability")
+    scale_color_manual(values = color_palette)+
+    labs(y = "Cumulative probability (Israel)")
   }else{
     ggplot(midBase, aes(x=NYSE_revenue)) + stat_ecdf(aes(color = afterAccidentDays,linetype = afterAccidentDays), 
                                                 geom = "step", size = 1.5) +
-      scale_color_manual(values = c("#00AFBB", "#E7B800"))+
-      labs(y = "Cumulative probability")
+      scale_color_manual(values = color_palette)+
+      labs(y = "Cumulative probability (USA)")
   }
   
   
